5cb1620b53268649c8ad0c07774879505b7eafb7
Marat Zaripov <49365339+GinoxXP@users.noreply.github.com>
2023-12-17 13:30:23 +0300

    Add destroying meshes

---
+++ Assets/Destruct/Scripts/BulletHitDestroy.cs
@@ -0,0 +1,30 @@
+﻿
+using Ginox.Pain.Weapon;
+using UnityEngine;
+
+namespace Ginox.Pain.Destruct
+{
+    public class BulletHitDestroy : MonoBehaviour
+    {
+        private MeshDestroy meshDestroy;
+        private BulletHitCollider bulletHitCollider;
+
+        private void OnHited()
+        {
+            meshDestroy.DestroyMesh();
+        }
+
+        private void Start()
+        {
+            meshDestroy = GetComponent<MeshDestroy>();
+            bulletHitCollider = GetComponent<BulletHitCollider>();
+
+            bulletHitCollider.Hited += OnHited;
+        }
+
+        private void OnDestroy()
+        {
+            bulletHitCollider.Hited -= OnHited;
+        }
+    }
+}
